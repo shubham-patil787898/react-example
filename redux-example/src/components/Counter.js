20a25e2994b9914a4d9e870c0b8f6fd80f8ec9dd
shubham-patil787898 <shubhampatil02322@gmail.com>
2024-08-01 21:45:16 +0530

    add redux example

---
+++ redux-example/src/components/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../actions/actions';
+
+const Counter = () => {
+    const count = useSelector((state) => state.count);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement
+</button>
+        </div>
+    );
+};
+
+export default Counter;
---
+++ redux-example/src/reducers/reducer.js
@@ -0,0 +1,23 @@
+import { INCREMENT, DECREMENT } from '../actions/actions';
+
+const initialState = {
+    count: 0
+};
+
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            };
+        default:
+return state;
+    }
+}
+export default counterReducer;
